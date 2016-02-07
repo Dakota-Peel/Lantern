63fc2a8d476f33c265684ca3c858e30e48c09720
Stephen Saunders <saunders.stephen@gmail.com>
2016-02-06 16:19:52 -0800

    Change the button to say 'Cancel Trip'

--- Client/Src/components/Settings.js
+++ Client/Src/components/Settings.js
@@ -70,7 +70,7 @@ export default class Settings extends Component {
 
     const showCheckin = user.onTrip ?
       <Button
-          text={'Checkin Now'}
+          text={'Cancel Trip'}
           onPress={() => this.props.navigator.push({name: 'passcodeConfirm'})}
           /> : null;
 
